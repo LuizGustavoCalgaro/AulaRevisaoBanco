4d6bc4adaeb27af5f56218e6014c0b17eb1c9616
LuizGustavoCalgaro <luiz270506@edu.unipar.br>
2025-08-27 19:58:25 -0300

    4 commit
    vai mais um

---
+++ AulaRevisaoBanco/src/main/kotlin/ui/Menu.kt
@@ -0,0 +1,8 @@
+package org.example.ui
+
+fun menu(){
+    println("1 - Cadastrar Caixa D' Àgua")
+    println("2 - Editar Caixa D' Àgua")
+    println("3 - Listar Caixa D' Àgua")
+    println("4 - Excluir Caixa D' Àgua")
+}
