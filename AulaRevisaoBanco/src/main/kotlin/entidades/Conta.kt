aaad258558974d1af5b0ed69a1ca843f8c59c49e
LuizGustavoCalgaro <luiz270506@edu.unipar.br>
2025-08-27 19:50:20 -0300

    3 commit
    agr vai

---
+++ AulaRevisaoBanco/src/main/kotlin/entidades/Conta.kt
@@ -0,0 +1,12 @@
+package entidades
+
+import java.math.BigDecimal
+
+class Conta (
+    val numeroConta : Long,
+    val titular: Pessoa,
+    var saldo : BigDecimal,
+    var receber : BigDecimal,
+    var saque : Double,
+    val agencia : Int
+);
